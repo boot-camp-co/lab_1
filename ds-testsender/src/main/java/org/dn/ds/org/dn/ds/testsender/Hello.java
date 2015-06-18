0d526bd68d14ec37c5cca3abe0bdd457714fd127
bryantwsmith <brysmith@redhat.com>
2015-06-18 10:42:58 -0400

    adds project for test sender

---
+++ ds-testsender/src/main/java/org/dn/ds/org/dn/ds/testsender/Hello.java
@@ -0,0 +1,10 @@
+package org.dn.ds.org.dn.ds.testsender;
+
+/**
+ * An interface for implementing Hello services.
+ */
+public interface Hello {
+
+    String hello();
+	
+}
---
+++ org.dn.ds.ordered/src/main/java/org/dn/ds/org/dn/ds/ordered/Hello.java
@@ -0,0 +1,10 @@
+package org.dn.ds.org.dn.ds.ordered;
+
+/**
+ * An interface for implementing Hello services.
+ */
+public interface Hello {
+
+    String hello();
+	
+}
